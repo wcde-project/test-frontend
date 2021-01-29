6350711e43cb47e412f2ee96c3046e5ab1bbd2ec
boh001 <boh001g@gmail.com>
2021-01-29 17:29:32 +0900

    text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ddfdff <code>src/App.js</code> and save to reload.
+         dfdqwer<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
